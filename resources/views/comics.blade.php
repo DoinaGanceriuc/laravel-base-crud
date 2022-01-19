61817fb1e291b1fc7d3293d7adc1a0ae4d9e3d4d
DoinaGanceriuc <doinaganceriuc@gmail.com>
2022-01-19 16:17:14 +0100

    add comics view

---
+++ resources/views/comics.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="site_main_content">
+    <div class="container mt-5">
+        <div class="row g-3 row-cols-1 row-cols-sm-3 row-cols-md-6 g-4">
+            @foreach ($comics as $comic)
+                <div class="col">
+                    <div class="card h-100">
+                        <img src="{{ $comic->thumb }}" class="card-img-top" alt="{{ $comic->title }}">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ strtoupper($comic->series) }} </h5>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+</div>
+  <!-- /.site_main_content -->
+@endsection
