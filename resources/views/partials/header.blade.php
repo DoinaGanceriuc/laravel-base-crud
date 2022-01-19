bd59e944a31a1953427631c99753c1a8732f6679
DoinaGanceriuc <doinaganceriuc@gmail.com>
2022-01-19 18:46:05 +0100

    create header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,38 @@
+<header id="site_header">
+        <div class="top">
+                <nav class="container">
+                <a class="pr-4" href="#">DC Power <span class="sigla">SM</span> VISA <i class="far fa-registered"></i></a>
+                <a href="#">Additional dc Sites <i class="fas fa-sort-down"></i></a>
+            </nav>
+        </div>
+    <div class="menu">
+        <div class="container">
+                <div class="logo">
+                    <a href="{{route('home')}}"><img src="{{asset('img/dc-logo.png')}}" alt="logo dc azzurro" width="60"></a>
+                </div>
+                <!-- /.logo -->
+                <div class="list">
+                    <ul>
+                    <li><a href="#">CHARACTERS</a></li>
+                    <li><a href="{{route('comics')}}">COMICS</a></li>
+                    <li><a href="">MOVIES</a></li>
+                    <li><a href="#">TV</a></li>
+                    <li><a href="#">GAMES</a></li>
+                    <li><a href="#">COLLECTIBLES</a></li>
+                    <li><a href="#">VIDEOS</a></li>
+                    <li><a href="#">FANS</a></li>
+                    <li><a href="#">NEWS</a></li>
+                    <li><a href="#">SHOP <i class="fas fa-sort-down"></i></a></li>
+                </ul>
+                </div>
+                <!-- /.list -->
+                <div class="search">
+                    <a class="list_active" href="#">Search <i class="fas fa-search"></i></a>
+                </div>
+                  <!-- /.search -->
+        </div>
+          <!-- /.container -->
+    </div>
+      <!-- /.menu -->
+
+</header>
