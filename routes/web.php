1fad9ac044456f19d0598fe1795654e819b6180e
DoinaGanceriuc <doinaganceriuc@gmail.com>
2022-01-19 17:04:57 +0100

    add function index in ComicController

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
  */
 
-Route::get('/', 'PageController@index')->name('home');
-Route::get('/comics', 'Admin\ComicController@index')->name('comics');
+Route::get('/', 'PageController@home')->name('home');
+Route::get('/comics', 'PageController@index')->name('comics');
+
+Route::get('/admin/comics', 'Admin\ComicController@index')->name('admin.comics.index');
