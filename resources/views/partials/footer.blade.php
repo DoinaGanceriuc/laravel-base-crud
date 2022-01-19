37944b279280d3c2db891c1712d40c1bbedb4417
DoinaGanceriuc <doinaganceriuc@gmail.com>
2022-01-19 18:46:11 +0100

    create footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,20 @@
+    <footer id="site_footer" class="fixed-bottom">
+      <div class="socials">
+        <div class="container">
+          <div class="btn">
+            <a href="">SIGN-UP NOW!</a>
+          </div>
+          <div class="left_icons">
+            <h4>FOLLOW US</h4>
+            <img src="{{asset('img/footer-facebook.png')}}" alt="logo grigio Facebook">
+            <img src="{{asset('img/footer-twitter.png')}}" alt="logo grigio Twitter">
+            <img src="{{asset('img/footer-youtube.png')}}" alt="logo grigio Youtube">
+            <img src="{{asset('img/footer-pinterest.png')}}" alt="logo grigio Pinterest">
+            <img src="{{asset('img/footer-periscope.png')}}" alt="logo grigio simbolo posizione">
+          </div>
+        </div>
+        <!-- /.container -->
+      </div>
+      <!-- /.socials -->
+    </footer>
+ <!-- /#site_footer -->
