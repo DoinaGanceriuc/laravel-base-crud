0a3c53b77cc854923b590cf910e441962252573e
DoinaGanceriuc <doinaganceriuc@gmail.com>
2022-01-19 16:14:53 +0100

    complete Comic migration and seeder

--- database/migrations/2022_01_19_140012_create_comics_table.php
+++ database/migrations/2022_01_19_140012_create_comics_table.php
@@ -15,6 +15,13 @@ class CreateComicsTable extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->longText('description')->nullable();
+            $table->string('thumb');
+            $table->string('price')->nullable();
+            $table->string('series');
+            $table->date('sale_date')->nullable();
+            $table->string('type')->nullable();
             $table->timestamps();
         });
     }
