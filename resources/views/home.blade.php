fca8a104ad0e954f059724f4d659e50586f58d81
DoinaGanceriuc <doinaganceriuc@gmail.com>
2022-01-19 16:40:58 +0100

    add PageController

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="site_main_content">
+    <div class="container mt-5">
+        <h1>Home Page DC Comics</h1>
+    </div>
+</div>
+  <!-- /.site_main_content -->
+@endsection
