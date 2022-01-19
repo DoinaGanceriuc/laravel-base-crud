001bf70e1b7a77ceb08e697392d3b8236e4692d1
DoinaGanceriuc <doinaganceriuc@gmail.com>
2022-01-19 17:04:12 +0100

    add function index in pageController

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,10 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comic;
+
 class PageController extends Controller
 {
-    public function index()
+    public function home()
     {
         return view('home');
+    }
+
+    public function index()
+    {
+        $comics = Comic::all();
+        return view('comics', compact('comics'));
     }
 }
