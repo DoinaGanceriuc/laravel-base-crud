af959f654c4c44d02937eb955ee666773d5ffd12
DoinaGanceriuc <doinaganceriuc@gmail.com>
2022-01-20 17:19:10 +0100

    add administrator link

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,7 +3,9 @@
                 <nav class="container">
                 <a class="pr-4" href="#">DC Power <span class="sigla">SM</span> VISA <i class="far fa-registered"></i></a>
                 <a href="#">Additional dc Sites <i class="fas fa-sort-down"></i></a>
+                <a class="ms-5" href="{{route('admin.comics.index')}}">Administrator</a>
             </nav>
+
         </div>
     <div class="menu">
         <div class="container">
