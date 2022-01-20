0c90ce4dd039a6347f9275b60562881d77485773
DoinaGanceriuc <doinaganceriuc@gmail.com>
2022-01-20 16:35:28 +0100

    fix @error in create.blade.php

--- resources/views/admin/comics/create.blade.php
+++ resources/views/admin/comics/create.blade.php
@@ -29,7 +29,7 @@
         </div>
         <div class="mb-3">
             <label for="thumb" class="form-label">Link immagine</label>
-            <input type="text" name="thumb" id="thumb" class="form-control @error('title') is-invalid @enderror" aria-describedby="thumbHelper" placeholder="Incolla il link dell'immagine" value="{{old('thumb')}}">
+            <input type="text" name="thumb" id="thumb" class="form-control @error('thumb') is-invalid @enderror" aria-describedby="thumbHelper" placeholder="Incolla il link dell'immagine" value="{{old('thumb')}}">
             @error('thumb')
              <div class="alert alert-danger">{{ $message }}</div>
             @enderror
@@ -40,14 +40,14 @@
         </div>
         <div class="mb-3">
             <label for="series" class="form-label">Serie Fumetto</label>
-            <input type="text" name="series" id="series" class="form-control @error('title') is-invalid @enderror" aria-describedby="seriesHelper" placeholder="" value="{{old('series')}}">
+            <input type="text" name="series" id="series" class="form-control @error('series') is-invalid @enderror" aria-describedby="seriesHelper" placeholder="" value="{{old('series')}}">
             @error('series')
              <div class="alert alert-danger">{{ $message }}</div>
             @enderror
         </div>
         <div class="mb-3">
             <label for="sale_date" class="form-label">Data di uscita</label>
-            <input type="text" name="sale_date" id="sale_date" class="form-control @error('title') is-invalid @enderror" aria-describedby="sale_dateHelper" placeholder="Indicare la data nel formato YYYY-MM-DD" value="{{old('sale_date')}}">
+            <input type="text" name="sale_date" id="sale_date" class="form-control @error('sale_date') is-invalid @enderror" aria-describedby="sale_dateHelper" placeholder="Indicare la data nel formato YYYY-MM-DD" value="{{old('sale_date')}}">
             @error('sale_date')
              <div class="alert alert-danger">{{ $message }}</div>
             @enderror
